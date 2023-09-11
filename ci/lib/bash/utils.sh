2100f82c8cf74524e3adb6f38b5b53c5e62fef75
Frédéric BARRAS HAMPEL <contact@bahl.fr>
2023-09-11 22:59:56 +0200

    misc(ci-utils): reformat log function

--- ci/lib/bash/utils.sh
+++ ci/lib/bash/utils.sh
@@ -20,26 +20,25 @@ else
 	export _color_reset=""
 fi
 
-# Exit with <code> and <msg>
-exit_failed() {
-	code=${1:-"1"}
-	msg=${1:-"script $0 failed unexpectedly"}
-
-	echo 1>&2 "$msg"
-	exit "$code"
-}
-
 # log to stderr, usage: log <level = info> <msg>
-log() {
+function log() {
 	level=${1:?Povide a valid log level, choices: i/I/info/INFO (warn,error,debug)}
 	shift
 	msg=${*:?You need to provide a <msg> as second argument}
 
 	case $level in
-	i | I | info | INFO) level="${_color_green}INFO${_color_reset}" ;;
-	w | W | warn | WARN) level="${_color_yellow}WARN${_color_reset}" ;;
-	e | E | error | ERROR) level="${_color_red}ERROR${_color_reset}" ;;
-	d | D | debug | DEBUG) level="${_color_blue}DEBUG${_color_reset}" ;;
+	i | I | info | INFO)
+		level="${_color_green}INFO${_color_reset}"
+		;;
+	w | W | warn | WARN)
+		level="${_color_yellow}WARN${_color_reset}"
+		;;
+	e | E | error | ERROR)
+		level="${_color_red}ERROR${_color_reset}"
+		;;
+	d | D | debug | DEBUG)
+		level="${_color_blue}DEBUG${_color_reset}"
+		;;
 	*)
 		log e "bad log level given, falling back to info"
 		level=INFO
