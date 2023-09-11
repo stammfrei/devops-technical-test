499823b189bfc13b6c03f690f6e611877aa3d3b3
Frédéric BARRAS HAMPEL <contact@bahl.fr>
2023-09-11 23:00:36 +0200

    feat(ci): add basic build function for wordpress

--- ci/build.sh
+++ ci/build.sh
@@ -68,8 +68,92 @@ function test-hw-img() {
 
 # Run the hello world image
 function run-hw-img() {
+	requires "docker"
+
 	log i "Running the hello world image"
 	docker run --rm -it "packer-hello-world:$(git rev-parse --short HEAD)" bash
+}
+
+# --- Wordpress build images
+# Build the wordpress image
+function build-wp() {
+	requires "packer" "git"
+
+	# Go back to project root at end of execution, the var must expand now
+	project_root="${PWD}"
+
+	tag=${1:-"emptytag"}
+	hw_folder="packer/wordpress"
+
+	# If explicit tag is not provided, use commit sha
+	if [ "$tag" == "emptytag" ]; then
+		log i "Using git sha as tag"
+		sha=$(git rev-parse --short HEAD)
+		tag=$sha
+	fi
+
+	# Export packer dynamic vars
+	export PKR_VAR_tag="$tag"
+
+	{
+		log i "Initialize packer"
+		packer init "$hw_folder"
+
+		log i "Validating and building packer files"
+		packer validate "$hw_folder" &&
+			packer build "$hw_folder/wordpress.pkr.hcl"
+	} || {
+		err_code="$?"
+		log e "failed to build image"
+		cd "${project_root}" || exit_failed 1 "Failed to go back to project root '${project_root}'"
+		return "$err_code"
+	}
+
+	cd "${project_root}" || exit_failed 1 "Failed to go back to project root '${project_root}'"
+}
+
+# Trigger build on file change for the hello world example
+function build-loop-wp() {
+	requires "entr" "find"
+	hw_folder="packer/wordpress"
+
+	find "$hw_folder" -type f |
+		SHELL=bash entr -cs "./ci/build.sh check &&./ci/build.sh build-wp && ./ci/build.sh test-wp"
+}
+
+# Test the hello world image
+function test-wp() {
+	requires "docker"
+
+	log i "Testing the wordpress image content"
+	docker run --rm -it "wordpress:$(git rev-parse --short HEAD)" \
+		"cd /opt && source ansible/bin/activate && ansible --help"
+}
+
+# Run the hello world image
+function run-wp() {
+	requires "docker"
+
+	log i "Running the wordpress image"
+	docker run --rm -it "wordpress:$(git rev-parse --short HEAD)" bash
+}
+
+# --- utils
+# lint and format files
+function check() {
+	requires "shfmt" "shellcheck" "grep"
+
+	log i "format sh files"
+	shfmt -w .
+
+	# lint bash files through shellcheck
+	# we identify then by the shebang
+	ci_files="$(grep -rE "^#.+/bin.+bash" ci -l)"
+	build_files="$(grep -rE "^#.+/bin.+bash" packer -l)"
+
+	log i "Checking sh files"
+	# shellcheck disable=2086
+	shellcheck $ci_files $build_files
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
